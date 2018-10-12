b592d15830e04c055d84795fcfca7db5f41ae77a
Bruus Riezebos <bruus@riezebos.net>
2018-10-12 13:42:48 +0200

    Huiswerk

--- Super-Wonder-Captain.py
+++ Super-Wonder-Captain.py
@@ -10,14 +10,20 @@ hash = hashlib.md5( (timestamp+private_key+public_key).encode('utf-8') )
 md5digest = str(hash.hexdigest())
 url = "http://gateway.marvel.com:80/v1/public/characters"
 connection_url = url+"?ts="+timestamp+"&apikey="+public_key+"&hash="+md5digest
-print(connection_url)
+#print(connection_url)
 response = requests.get(connection_url)
 jsontext = json.loads(response.text) # om de JSON leesbaar te printen... #
 
-print(json.dumps(jsontext, sort_keys=True, indent=4))
-print("\nGevonden characters in comics:") # JSON-indeling kun je uit het geprinte resultaat halen of uit de Marvel docs!
+#print(json.dumps(jsontext, sort_keys=True, indent=4))
+#print("\nGevonden characters in comics:") # JSON-indeling kun je uit het geprinte resultaat halen of uit de Marvel docs!
 for item in jsontext['data']['results'][0]['comics']['items']:
-    print(item['name'])
-    print("\nGevonden characters in series:") # JSON-indeling kun je uit het geprinte resultaat halen of uit de Marvel docs!
+#    print(item['name'])
+#    print("\nGevonden characters in series:") # JSON-indeling kun je uit het geprinte resultaat halen of uit de Marvel docs!
     for item in jsontext['data']['results'][0]['series']['items']:
         print(item['name'])
+#print(jsontext['data']['results'][16])
+list = jsontext['data']['results']
+
+for i in list:
+    if i['name'] == "Aginar":
+        print(i['name'])
